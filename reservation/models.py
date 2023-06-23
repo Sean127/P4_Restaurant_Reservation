86d83ab68afea26d339e7c925cc9d3153134da18
Sean127 <sdoran401@gmail.com>
2023-06-23 15:28:31 +0000

    updated model for reservation

--- reservation/models.py
+++ reservation/models.py
@@ -1,17 +1,38 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from datetime import datetime
 
 
 # Create your models here.
+TIME_CHOICES = (
+   ("5 PM", "5 PM"),
+   ("5:30 PM", "5:30 PM"),
+   ("6 PM", "6 PM"),
+   ("6:30 PM", "6:30 PM"),
+   ("7 PM", "7 PM"),
+   ("7:30 PM", "7:30 PM"),
+   ("8 PM", "8 PM"),
+   ("8:30 PM", "8:30 PM"),
+   ("9 PM", "9 PM")
+)
+
+PEOPLE = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+    ("6", "6"),
+    ("7", "7"),
+    ("8", "8"),
+)
+
 
 class Reservation(models.Model):
-    name = models.CharField(max_length=20)
-    date = models.DateField()
-    time = models.TimeField()
-    people = models.PositiveIntegerField()
+    name = models.CharField(max_length=20,)
+    date = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="5 PM")
+    people = models.CharField(max_length=5, choices=PEOPLE, default="1")
 
     def __str__(self):
         return self.name
-
-    def no_of_people(self):
-        return self.people

fbcf17d07dbaa94896bdab11d4399c06f41a11b2
Sean127 <sdoran401@gmail.com>
2023-06-23 13:26:23 +0000

    Add admin panel with basic model

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+class Reservation(models.Model):
+    name = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    people = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def no_of_people(self):
+        return self.people
